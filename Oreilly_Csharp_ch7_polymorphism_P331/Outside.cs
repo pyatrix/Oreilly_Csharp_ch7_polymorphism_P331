4d54bb1aa17b282132cf95e5ee94423a73dc6d8e
Peng <Peng@PENG-PC>
2019-05-14 15:58:53 +0800

    P332-1 finish

---
+++ Oreilly_Csharp_ch7_polymorphism_P331/Outside.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oreilly_Csharp_ch7_polymorphism_P331
+{
+    class Outside
+    {
+    }
+}
