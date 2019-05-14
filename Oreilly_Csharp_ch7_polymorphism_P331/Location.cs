59dfd806006f364c0612b566fe74798f24f7aead
Peng <Peng@PENG-PC>
2019-05-14 17:09:31 +0800

    P333-4

--- Oreilly_Csharp_ch7_polymorphism_P331/Location.cs
+++ Oreilly_Csharp_ch7_polymorphism_P331/Location.cs
@@ -8,8 +8,22 @@ namespace Oreilly_Csharp_ch7_polymorphism_P331
 {
     abstract class Location
     {
-        public string Name;
+        public string Name { get; private set; }
         public Location[] Exits;
-        virtual public string Description { get; }
+        virtual public string Description
+        {
+            get
+            {
+                string description = "You're standing in the " + Name + ". You see exits to the following places:";
+                for (int i = 0; i < Exits.Length; i++)
+                {
+                    description += " " + Exits[i].Name;
+                    if (i != Exits.Length - 1)
+                        description += ",";
+                }
+                description += ".";
+                return description;
+            }
+        }
     }
 }
