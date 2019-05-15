e8fdf079298e51b097f08c8996424c5ffc4537a9
Peng <Peng@PENG-PC>
2019-05-15 14:51:44 +0800

    P338 finish

--- Oreilly_Csharp_ch7_polymorphism_P331/Form1.cs
+++ Oreilly_Csharp_ch7_polymorphism_P331/Form1.cs
@@ -12,27 +12,37 @@ namespace Oreilly_Csharp_ch7_polymorphism_P331
 {
     public partial class Form1 : Form
     {
+        //用於記錄當前場所
+        Location currentLocation;
+
+        //表單使用這些參考變數紀錄每個場所
+        RoomWithDoor livingRoom;
+        Room diningRoom;
+        RoomWithDoor kitchen;
+        OutsideWithDoor frontYard;
+        OutsideWithDoor backYard;
+        Outside garden;
+
         public Form1()
         {
             InitializeComponent();
             CreateObjects();
-            currentLocation
         }
         
         private void CreateObjects()
         {
-            //............................name,....hot
-            Outside garden = new Outside("Garden", false);
-            //..............................................name,.......hot,....decoration
-            OutsideWithDoor backYard = new OutsideWithDoor("Back Yard", true, " a screen door ");
-            //...............................................name,........hot,.....decoration
-            OutsideWithDoor frontYard = new OutsideWithDoor("Front Yard", false, " an oak door with a brass knob");
-            //..........................................name,..........decoration,............Door Description
-            RoomWithDoor livingRoom = new RoomWithDoor("Living Room", "an antique carpet ", " an oak door with a brass knob");
-            //..........................name,..........decoration
-            Room diningRoom = new Room("Dining Room", "a crystal chandelier");
-            //.......................................name,.......decoration,.....................Door Description
-            RoomWithDoor kitchen = new RoomWithDoor("Kitchen", " a stainless steel appliance", " a screen door ");
+            //....................name,....hot
+            garden = new Outside("Garden", false);
+            //..............................name,.......hot,....decoration
+            backYard = new OutsideWithDoor("Back Yard", true, " a screen door ");
+            //...............................name,........hot,.....decoration
+            frontYard = new OutsideWithDoor("Front Yard", false, " an oak door with a brass knob");
+            //.............................name,..........decoration,............Door Description
+            livingRoom = new RoomWithDoor("Living Room", "an antique carpet ", " an oak door with a brass knob");
+            //.....................name,..........decoration
+            diningRoom = new Room("Dining Room", "a crystal chandelier");
+            //..........................name,......decoration,..................Door Description
+            kitchen = new RoomWithDoor("Kitchen", "stainless steel appliance", "a screen door");
 
             //6個objects都繼承抽象類別Location，因此對Exits填值
             //Exits是Location類別里的public字串陣列欄位，不應用這種方式封裝。
@@ -43,6 +53,12 @@ namespace Oreilly_Csharp_ch7_polymorphism_P331
             livingRoom.Exits = new Location[] { frontYard, diningRoom };
             diningRoom.Exits = new Location[] { livingRoom, kitchen };
             kitchen.Exits = new Location[] { diningRoom, backYard };
+
+            //對IHasExteriorDoor來說，必須為它們設定外門另一側的場所
+            livingRoom.DoorLocation = frontYard;
+            frontYard.DoorLocation = livingRoom;
+            kitchen.DoorLocation = backYard;
+            backYard.DoorLocation = kitchen;
         }
     }
 }
